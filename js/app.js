89ade38e1d3fa54626e24df7cce6e72335021ca3
Rachel <R.FREEMAN-2020@hull.ac.uk>
2020-11-26 21:44:21 +0000

    update2

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ function imagefun() {
 
         var Image_Id = document.getElementById('getImage');
         if (Image_Id.src.match("./pic/pic7.jpg")) {
-             Image_Id.src = "./pic/pic8.jpg";
+             Image_Id.src = "./pic/pic8.jpg" ;
         }
         else {
             Image_Id.src = "./pic/pic7.jpg";
